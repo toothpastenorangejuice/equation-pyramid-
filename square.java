7e288f2f4def05e39c3dfe512782e4bcfb5db8b3
MosaSelela <145341687+MosaSelela@users.noreply.github.com>
2026-05-11 21:42:18 +0200

    Creating some classes

---
+++ square.java
@@ -0,0 +1,14 @@
+//this will be responsible for a singular tile 
+public class square {
+
+    private char label;
+    private char operator;
+    private int value; 
+
+    public square(char label, char operator, int value){
+        this.label = label; 
+        this.operator = operator; 
+        this.value = value; 
+    }
+    
+}
